f83a2b664ef822bd34a877aada3b855e43024caf
Sandrovichus <veber21@rambler.ru>
2024-02-14 14:16:44 +0300

    Randoms

---
+++ src/Randoms.java
@@ -0,0 +1,30 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    private final int min;
+    private final int max;
+    public Randoms(int min, int max) {
+        random = new Random();
+        this.min = min;
+        this.max = max;
+    }
+    @NotNull
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(min, max + 1);
+            }
+        };
+    }
+}
